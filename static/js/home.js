d072d787d058deb33240719b60711ab3d6738319
mzehra-1512 <m.zehra1512@gmail.com>
2025-11-10 19:12:53 +0400

    Update home.js

--- static/js/home.js
+++ static/js/home.js
@@ -2,12 +2,10 @@ function toggleReadMore(btn) {
     const contentDiv = btn.previousElementSibling;
 
     if (!contentDiv.classList.contains("expanded")) {
-        // Expand: show full note from data-full
         contentDiv.innerHTML = contentDiv.dataset.full;
         contentDiv.classList.add("expanded");
         btn.textContent = "Show Less";
     } else {
-        // Collapse: show truncated preview
         let preview = contentDiv.dataset.full;
         if(preview.length > 100) {
             preview = preview.slice(0, 100) + '...';
