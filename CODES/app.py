62031888976c43daa5f5a9b785e043edd1ada409
mzehra-1512 <m.zehra1512@gmail.com>
2025-11-10 09:37:49 +0400

    Update app.py

--- CODES/app.py
+++ CODES/app.py
@@ -1,7 +1,26 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, jsonify
+import json, os
 
 #--Flask App Setup--
-app = Flask(_name_)
+app = Flask(__name__)
+
+#Path to notes data file
+DATA_PATH = os.path.join("data", "notes.json")
+
+#--Helper Functions--
+def load_notes():
+     if not os.path.exists(DATA_PATH):
+          return[]
+     with open(DATA_PATH, "r") as file:
+          try: 
+               return json.load(file)
+          except json.JSONDecodeError:
+               return[]
+          
+def save_notes(notes):
+     with open(DATA_PATH, "w") as file:
+          json.dump(notes, file, indent =4)
+
 
 #--Routes for pages-- 
 @app.route('/')
@@ -10,7 +29,7 @@ def home():
 
 @app.route('/notes')
 def notes():
-    return render_template('notes.html')
+    return render_template('notes.html', notes=notes_list)
 
 @app.route('/todo')
 def todo():
@@ -24,7 +43,25 @@ def pomodoro():
 def planner():
     return render_template('planner.html')
 
+#--API for saving notes--
+@app.route('/save_note', methods=['POST'])
+def save_notes():
+     data = request.get_json()
+     notes = load_notes()
+     new_note = {
+          "content" : data["content"],
+          "date" : data["date"]
+     }
+     notes.append(new_note)
+     save_note(notes)
+     return jsonify({"message": "Note saved successfully!"})
 #--Run the app--
-if_name_=='_main_':
-	app.run(debug=True)
+if __name__== '__main__':
+     os.makedirs("data", exist_ok=True)
+     if not os.path.exists(DATA_PATH):
+          save_notes([])
+     app.run(debug=True)
+
+    
+
 	
